29643960e6b8102d7b9ae93276950699da72a0f3
Azumah Ebenezer Adelwin <azumahebenezer1@gmail.com>
2021-09-02 10:57:07 +0000

    style(lint): fix linting error

--- src/@shared/helpers/__tests__/getIconByType.test.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/react-native';
-import getIconByType from '../getIconByType';
-
-test('Get icon by type', () => {
-  const icon = getIconByType('Ionicons');
-  //   expect(getIconByType).toHaveBeenCalled();
-  //   expect(getIconByType).toHaveBeenCalledWith('Ionicons');
-});
--- src/@shared/helpers/getIconByType.ts
+++ src/@shared/helpers/getIconByType.ts
@@ -5,38 +5,38 @@ const getIconByType = (type?: IconType) => {
   switch (type) {
     case 'MaterialIcons':
       return React.lazy(
-        () => import(`react-native-vector-icons/MaterialIcons`),
+        () => import('react-native-vector-icons/MaterialIcons'),
       );
     case 'SimpleLineIcons':
       return React.lazy(
-        () => import(`react-native-vector-icons/SimpleLineIcons`),
+        () => import('react-native-vector-icons/SimpleLineIcons'),
       );
     case 'MaterialCommunityIcons':
       return React.lazy(
-        () => import(`react-native-vector-icons/MaterialCommunityIcons`),
+        () => import('react-native-vector-icons/MaterialCommunityIcons'),
       );
     case 'Zocial':
-      return React.lazy(() => import(`react-native-vector-icons/Zocial`));
+      return React.lazy(() => import('react-native-vector-icons/Zocial'));
     case 'Entypo':
-      return React.lazy(() => import(`react-native-vector-icons/Entypo`));
+      return React.lazy(() => import('react-native-vector-icons/Entypo'));
     case 'Feather':
-      return React.lazy(() => import(`react-native-vector-icons/Feather`));
+      return React.lazy(() => import('react-native-vector-icons/Feather'));
     case 'Octicons':
-      return React.lazy(() => import(`react-native-vector-icons/Octicons`));
+      return React.lazy(() => import('react-native-vector-icons/Octicons'));
     case 'Ionicons':
-      return React.lazy(() => import(`react-native-vector-icons/Ionicons`));
+      return React.lazy(() => import('react-native-vector-icons/Ionicons'));
     case 'AntDesign':
-      return React.lazy(() => import(`react-native-vector-icons/AntDesign`));
+      return React.lazy(() => import('react-native-vector-icons/AntDesign'));
     case 'EvilIcons':
-      return React.lazy(() => import(`react-native-vector-icons/EvilIcons`));
+      return React.lazy(() => import('react-native-vector-icons/EvilIcons'));
     case 'Foundation':
-      return React.lazy(() => import(`react-native-vector-icons/Foundation`));
+      return React.lazy(() => import('react-native-vector-icons/Foundation'));
     case 'FontAwesome':
-      return React.lazy(() => import(`react-native-vector-icons/FontAwesome`));
+      return React.lazy(() => import('react-native-vector-icons/FontAwesome'));
     case 'FontAwesome5':
-      return React.lazy(() => import(`react-native-vector-icons/FontAwesome5`));
+      return React.lazy(() => import('react-native-vector-icons/FontAwesome5'));
     default:
-      return React.lazy(() => import(`react-native-vector-icons/Ionicons`));
+      return React.lazy(() => import('react-native-vector-icons/Ionicons'));
   }
 };
 
